3a0dbf86594e17f250ff6541fa1c5cc71c91443b
JohanAanesen <johanaan@stud.ntnu.no>
2018-09-10 14:00:40 +0200

    adding rows n shi is now done more programatically :)

--- view/home/index.php
+++ view/home/index.php
@@ -7,8 +7,34 @@ $twig   = Server::requireTwig();
 
 
 
+$test = null;
+$test[0] = new test();
+$test[1] = new test();
+$test[2] = new test();
+$test[3] = new test();
+
+
 echo $twig->render('home.html', array(
-    'title' => 'Home'
+    'title' => 'Home',
+    'forumTableRows' => $test,
 ));
 
 
+class test{
+    public $title = null;
+    public $description = null;
+    public $topics = null;
+    public $posts = null;
+    public $lastPostTitle = null;
+    public $lastPostAuthor = null;
+    public $lastPostTimestamp = null;
+    public function __construct(){
+        $this->title = "Johan er flink ass";
+        $this->description = "test";
+        $this->topics = "test";
+        $this->posts = "test";
+        $this->lastPostTitle = "test";
+        $this->lastPostAuthor = "test";
+        $this->lastPostTimestamp = "test";
+    }
+}

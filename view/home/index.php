f670db97438f56953e684ce8ebab55e9a16019f1
JohanAanesen <johanaan@stud.ntnu.no>
2018-10-05 13:48:18 +0200

    hotfix

--- view/home/index.php
+++ view/home/index.php
@@ -6,7 +6,7 @@ require_once __DIR__ . "/../../config.php";
 $db     = Server::requireDatabase();
 $twig   = Server::requireTwig();
 
-$app = new Application();
+$app = Application::get_instance();
 
 $loggedIn = false;
 
